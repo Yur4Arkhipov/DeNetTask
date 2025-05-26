7ed07acdc7d1f0dcf7dfa8c32361e89545b6ea69
Yur4Arkhipov <yura4rkhipov@yandex.ru>
2025-05-26 20:35:00 +0300

    new logic with create new mode and remove node

--- app/src/main/java/com/example/danettask/data/TreeNode.kt
+++ app/src/main/java/com/example/danettask/data/TreeNode.kt
@@ -1,12 +1,14 @@
 package com.example.danettask.data
 
+import androidx.compose.runtime.mutableStateListOf
+
 /**
  * Node с названием, детьми и ссылкой на родителя
  */
 class TreeNode<T>(
     val name: T
 ) {
-    private val _children: MutableList<TreeNode<T>> = mutableListOf()
+    private val _children: MutableList<TreeNode<T>> = mutableStateListOf<TreeNode<T>>()
     var parent: TreeNode<T>? = null
         private set
 
@@ -16,5 +18,10 @@ class TreeNode<T>(
     fun addChild(child: TreeNode<T>) {
         child.parent = this
         _children.add(child)
+    }
+
+    fun removeChild(child: TreeNode<T>) {
+        _children.remove(child)
+        child.parent = null
     }
 }
--- app/src/main/java/com/example/danettask/ui/TreeScreen.kt
+++ app/src/main/java/com/example/danettask/ui/TreeScreen.kt
@@ -1,23 +1,36 @@
 package com.example.danettask.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.danettask.R
 import com.example.danettask.data.TreeNode
 import com.example.danettask.data.findNodeByPath
@@ -30,9 +43,10 @@ fun <T> TreeScreen(
     root: TreeNode<T>,
     converter: TreeTypeConverter<T>,
     onNavigateToChild: (String) -> Unit,
-    onBack: () -> Unit
+    onBack: () -> Unit,
 ) {
     val currentNode = root.findNodeByPath(nodePath) { converter.fromString(it) }
+    var newNode by remember { mutableStateOf("") }
 
     Scaffold(
       topBar = {
@@ -53,30 +67,93 @@ fun <T> TreeScreen(
           )
       }
     ) { innerPadding ->
-        Column(
-            verticalArrangement = Arrangement.Center,
+        LazyColumn(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
-            Text("Current node: ${currentNode?.name}")
-
-            Spacer(Modifier.height(20.dp))
-
-            currentNode?.children?.forEach { child ->
-                Button(
-                    onClick = {
-                        val newPath = "$nodePath/${child.name}"
-                        onNavigateToChild(newPath)
-                    }
-                ) {
-                    Text(child.name.toString())
+            currentNode?.let { node ->
+                item {
+                    Text("Current node: ${node.name}")
+                    Spacer(Modifier.height(20.dp))
                 }
-            }
 
-            if (currentNode?.children.isNullOrEmpty()) {
-                Text("No children")
+                if (node.children.isEmpty()) {
+                    item {
+                        Text("No children")
+                        Spacer(Modifier.height(10.dp))
+                    }
+                } else {
+                    items(node.children) { child ->
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Button(
+                                onClick = {
+                                    val newPath = "$nodePath/${child.name}"
+                                    onNavigateToChild(newPath)
+                                }
+                            ) {
+                                Text(converter.toString(child.name))
+                            }
+
+                            IconButton(
+                                onClick = {
+                                    node.removeChild(child)
+                                }
+                            ) {
+                                Icon(
+                                    painter = painterResource(R.drawable.baseline_delete_24),
+                                    contentDescription = null
+                                )
+                            }
+                        }
+                        Spacer(Modifier.height(10.dp))
+                    }
+                }
+
+                item {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        OutlinedTextField(
+                            value = newNode,
+                            onValueChange = { newNode = it },
+                            label = {
+                                Text(
+                                    text = "Add new child",
+                                    fontSize = 10.sp,
+                                )
+                            },
+                            keyboardOptions = KeyboardOptions.Default.copy(
+                                imeAction = ImeAction.Done
+                            ),
+                            keyboardActions = KeyboardActions(
+                                onDone = { /* закрыть клавиатуру, если хочешь */ }
+                            ),
+                        )
+
+                        IconButton(
+                            onClick = {
+                                val value = converter.fromString(newNode)
+                                if (value != null) {
+                                    node.addChild(TreeNode(value))
+                                    newNode = ""
+                                }
+                            },
+                            modifier = Modifier.padding(top = 8.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Add,
+                                contentDescription = null
+                            )
+                        }
+                    }
+                }
+            } ?: item{
+                Text("Node not found")
             }
         }
     }
